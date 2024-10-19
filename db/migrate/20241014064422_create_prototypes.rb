ec39535943affb09e71b265980f1951c9b4c550e
rita-snow <yummy.standrews@gmail.com>
2024-10-19 15:36:16 +0900

    マイグレーションファイルの記述削除

--- db/migrate/20241014064422_create_prototypes.rb
+++ db/migrate/20241014064422_create_prototypes.rb
@@ -1,7 +1,6 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      validates :image, presence: true
       t.text :catch_copy,             null: false
       t.text :concept,                null: false
       t.references :user,             null: false,  foreign_key: true
