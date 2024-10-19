465d3f44b7289b8e1e4728b5375bc8af362c1fb3
rita-snow <yummy.standrews@gmail.com>
2024-10-19 16:48:54 +0900

    prototypesマイグレーションファイルの修正（追加したカラムの削除）

--- db/migrate/20241019073352_add_fields_to_prototypes.rb
+++ db/migrate/20241019073352_add_fields_to_prototypes.rb
@@ -1,8 +1,5 @@
 class AddFieldsToPrototypes < ActiveRecord::Migration[7.0]
   def change
     add_column :prototypes, :title, :string
-    add_column :prototypes, :catch_copy, :string
-    add_column :prototypes, :concept, :text
-    add_column :prototypes, :image, :string
   end
 end
