0b7eec3f1790a6f086ed7ea4fd6abe26261dfb43
rita-snow <yummy.standrews@gmail.com>
2024-10-19 16:15:50 +0900

    prototypeコントローラー、マイグレーションファイルの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -50,5 +50,5 @@ end
   private
 
   def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :user, :image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title, :catch_copy, :concept,:image).merge(user_id: current_user.id)
   end
